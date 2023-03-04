84a86693a60e258915e0c3b6ab294dd1189d6960
Fernanda <26352882+fernandakagami@users.noreply.github.com>
2023-03-04 08:58:28 -0300

    Create SegundoController.php

---
+++ htdocs/app/code/local/CursoMagento/PrimeiroController/controllers/Pasta/SegundoController.php
@@ -0,0 +1,9 @@
+<?php
+
+class CursoMagento_PrimeiroController_pasta_SegundoController extends CursoMagento_PrimeiroController_Controller_Front_Action
+{
+    public function segundaAction()
+    {
+        $this->printarNome();
+    }
+}
