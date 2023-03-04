f5ba430f7c0c3df8f1b2062eece884fa0c382155
Fernanda <26352882+fernandakagami@users.noreply.github.com>
2023-03-04 08:58:16 -0300

    Update Action.php

--- htdocs/app/code/local/CursoMagento/PrimeiroController/Controller/Front/Action.php
+++ htdocs/app/code/local/CursoMagento/PrimeiroController/Controller/Front/Action.php
@@ -3,5 +3,8 @@
 class CursoMagento_PrimeiroController_Controller_Front_Action extends Mage_Core_Controller_Front_Action
 {
 
-    
+    public function printarNome()
+    {
+        echo 'Fernanda';
+    }
 }
