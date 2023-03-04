63fc654ea94e0f357af23c9f018a45e4d024bfcb
Fernanda <26352882+fernandakagami@users.noreply.github.com>
2023-03-04 08:58:22 -0300

    Update MeucontrollerController.php

--- htdocs/app/code/local/CursoMagento/PrimeiroController/controllers/MeucontrollerController.php
+++ htdocs/app/code/local/CursoMagento/PrimeiroController/controllers/MeucontrollerController.php
@@ -5,6 +5,16 @@ class CursoMagento_PrimeiroController_MeucontrollerController extends CursoMagen
 
     public function minhaactionAction()
     {
+        $this->printarNome();
+
+        // if (!$this->getRequest()->isPost()) {
+        //    $this->_redirect('*/*/actionredirect');
+        //    return;
+        //}        
+
+        // $this->_forward('actionforward', 'meucontroller', 'primeirocontroller' . array());
+
+        /* 
         $params = $this->getRequest()->getParams();
 
         var_dump($params);
@@ -18,11 +28,27 @@ class CursoMagento_PrimeiroController_MeucontrollerController extends CursoMagen
             $post = $this->getRequest()->getPost();
         }
 
-        if ($this->getRequest()->isSecure()) {
-            /** ... */
+        if ($this->getRequest()->isSecure()) {          
         }
 
+        $this->getResponse()->setBody('Fernanda');
+
         echo 'Oi, meu nome é ' . $name . ' e meu sobrenome é ' . $lastname;
+
+        $this->loadLayout();
+
+        $this->renderLayout();
+        */
+    }
+
+    public function actionforwardAction()
+    {
+        echo 'Action Forward';
+    }
+
+    public function actionredirectAction()
+    {
+        echo 'Action Redirect';
     }
     
 }
